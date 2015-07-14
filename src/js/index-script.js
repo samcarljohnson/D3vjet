9df2aa70bd946d75b314be57f0e07f6e0da3dcf2
Sam Johnson <samcarljohnson@gmail.com>
2015-07-14 16:12:59 -0400

    first commit

---
+++ src/js/index-script.js
@@ -0,0 +1,13 @@
+var dataset = [5, 10, 15, 20, 25];
+var d3magic = function() {
+  d3.select("body").selectAll("div")
+  .data(dataset)
+  .enter()
+  .append("div")
+  .attr("class", "bar")
+  .style("height", function(d) {
+	 return d + "px"; 
+  });
+}
+
+	    
